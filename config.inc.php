1b21ebd55ba173c5b3dd915a4740a6472a69f0ae
soujanya <soujanya@soujanya-laptop.(none)>
2014-11-19 15:32:33 +0530

    commiting sources

---
+++ config.inc.php
@@ -0,0 +1,11 @@
+<?php
+# Database name
+$db = "emt";
+# Internet address or hostname of database host
+$db_host = "localhost";
+# Database username
+$db_user = "emt";
+# Database password
+$db_password = "changethis";
+
+?>
